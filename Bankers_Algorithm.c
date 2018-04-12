bd156d127fb5a1a42bf01f4d34d5798c13428d54
chandicharan5983 <38296507+chandicharan5983@users.noreply.github.com>
2018-04-13 01:25:55 +0530

    Update Bankers_Algorithm.c

--- Bankers_Algorithm.c
+++ Bankers_Algorithm.c
@@ -1,44 +1,66 @@
-
 #include<conio.h>
 #include<stdio.h>
 int main()
 {
-	int allocation[10][10],max[10][10],need[10][10],av[10],n,i,j,k;
+	int allocation[10][10],max[10][10],need[10][10],av[10],n,i,j,k,count=0;
 	printf("Enter the number of processers:\n");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-		printf("Enter the Allocation value of Processer P%d as A  B  C  D\n",&i+1);
+		printf("Enter the Allocation value of Processer P%d as A  B  C  D\n", i+1);
 		for(j=0;j<4;j++)
 		{
 			scanf("%d",&allocation[i][j]);
 		}
 	}
+	printf("\n\n\n");
 	for(i=0;i<n;i++)
 	{
-		printf("Enter the Max value of Processer P%d as A  B  C  D\n",&i+1);
+		printf("Enter the Max value of Processer P%d as A  B  C  D\n", i+1);
 		for(j=0;j<4;j++)
 		{
 			scanf("%d",&max[i][j]);
 		}
 	}
+	printf("\n\n\n");
 	printf("Enter the value of Available as A B C D\n");
-	for(k=0;k<n;k++)
+	for(k=0;k<4;k++)
 	{
 		scanf("%d",&av[k]);
 	}
+	printf("\n\n\n");
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<4;j++)
 		{
-			need[i][j]=max[i][j]-allocation[i][j];
+			need[i][j]=max[i][j] - allocation[i][j];
 		}
 	}
-	for(i=0;i<n;i++)
+	for(i=0;i%n<n;i++)
 	{
+		i = i%n;
 			if(need[i][0] <= av[0] && need[i][1] <= av[1] && need[i][2] <= av[2] && need[i][3] <= av[3])
+		{
+			printf("P%d is processes:\n", i+1);
+			av[0]+=allocation[i][0];
+			av[1]+=allocation[i][1];
+			av[2]+=allocation[i][2];
+			av[3]+=allocation[i][3];
+			need[i][0] = 999;
+			need[i][1] = 999;
+			need[i][2] = 999;
+			need[i][3] = 999;
+			count++;
+			if(count==n)
 			{
-				printf("P%d is processes:\n", i+1);
+				printf("This system is in safe state\n");
+				break;
 			}
+			else
+			{
+				printf("This system is in safe state\n");
+				break;
+			}
+		}
 	}	
 }
